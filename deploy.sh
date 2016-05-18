4212688410a520ee50418152417ac98862c1db19
René Filip <renefilip@mail.com>
2016-05-18 22:39:40 +0200

    add basic docker hub push

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,11 @@
 trap 'exit 2' ERR
 
 echo "deploy .."
+
+# let's do something simple first. later see: https://sebest.github.io/post/using-travis-ci-to-build-docker-images/
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker tag gameserver filipre/gameserver:latest
+docker tag gameserver filipre/gameserver:travis-$TRAVIS_BUILD_NUMBER
+docker push filipre/gameserver
+
+echo "OK"
