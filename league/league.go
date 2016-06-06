df9bfefaa084472110cd57d9029a455b7d1cdb3e
René Filip <renefilip@mail.com>
2016-06-06 22:07:13 +0200

    create future packages to implement

---
+++ league/league.go
@@ -0,0 +1,17 @@
+package league
+
+//
+// type League struct {
+// 	Id      uuid.UUID
+// 	Name    string
+// 	UrlName string
+// 	Status  LeagueStatus
+// 	Changed time.Time
+// }
+// type LeagueStatus string
+//
+// const (
+// 	LeagueStarted   LeagueStatus = "started"
+// 	LeagueDissolved LeagueStatus = "dissolved"
+// )
+//
---
+++ member/member.go
@@ -0,0 +1,17 @@
+package member
+
+// type Member struct {
+// 	Id      uuid.UUID
+// 	Player  uuid.UUID
+// 	League  uuid.UUID
+// 	Role    MemberRole
+// 	Changed time.Time
+// }
+// type MemberRole string
+//
+// const (
+// 	MemberMember MemberRole = "member"
+// 	MemberBanned MemberRole = "banned"
+// 	MemberAdmin  MemberRole = "admin"
+// 	MemberOwner  MemberRole = "owner"
+// )
